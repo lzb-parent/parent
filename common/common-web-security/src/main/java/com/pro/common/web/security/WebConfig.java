1c920bedaf4940b9db1bf7b6b86ee44b85f38444
qaz4042 <404286846@qq.com>
2024-12-11 23:28:38 +0800

    fix: 修复 mysql8 java-to-db mainLength 不匹配一堆sql执行

--- common/common-web-security/src/main/java/com/pro/common/web/security/WebConfig.java
+++ common/common-web-security/src/main/java/com/pro/common/web/security/WebConfig.java
@@ -55,45 +55,6 @@ public class WebConfig implements WebMvcConfigurer {
     private TokenService tokenService;
 
     /**
-     * 解决 ID_WORKER 生成主键太长导致 js 精度丢失（JackJson 处理方式）
-     */
-    @Bean
-    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder, @Value("${spring.application.name}") String springApplicationName) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
-        // Include.Include.ALWAYS 默认
-        // Include.NON_DEFAULT 属性为默认值不序列化
-        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
-        // Include.NON_NULL 属性为NULL 不序列化,就是为null的字段不参加序列化
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        // 解决,页面通过@RequestBody传多余的属性时,报错不存在属性问题
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        // 解决,JavaScript 无法处理 Java 的长整型 Long 导致精度丢失，具体表现为主键最后两位永远为 0，解决思路： Long 转为 String 返回
-        SimpleModule simpleModule = new SimpleModule();
-        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        // 时间转换
-        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        simpleModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-        simpleModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        simpleModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        simpleModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-//          simpleModule.addDeserializer(Enum.class, new EnumDeserializer());
-
-        if (EnumApplication.user.equals(commonProperties.getApplication())) {
-            // 富文本无法插入
-            simpleModule.addDeserializer(String.class, new JsonHtmlXssDeserializer(String.class));
-        }
-        objectMapper.registerModule(simpleModule);
-
-        // 初始化json工具
-        JSONUtils.init(objectMapper);
-
-        return objectMapper;
-    }
-
-    /**
      * 获取登录信息token
      */
     @Override
