b98e59a94e4518eaab39aa36cd448ddd88bfb1f6
qaz4042 <404286846@qq.com>
2025-01-01 22:08:34 +0800

    feat: 其他无用代码和配置待删减 配置待固化

--- common/common-module-service-dependencies/src/main/java/com/pro/common/modules/service/dependencies/util/upload/FileUploadUtils.java
+++ common/common-module-service-dependencies/src/main/java/com/pro/common/modules/service/dependencies/util/upload/FileUploadUtils.java
@@ -1,6 +1,7 @@
 package com.pro.common.modules.service.dependencies.util.upload;
 
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.crypto.SecureUtil;
 import com.pro.common.modules.api.dependencies.exception.BusinessException;
@@ -58,17 +59,23 @@ public class FileUploadUtils {
 
         LocalDateTime time = LocalDateTime.now();
         UploadModuleModel finalModule = module;
-        return Arrays.stream(files).map(file -> uploadFile(new UploadVo(file), finalModule, time)).collect(Collectors.toList());
+        return Arrays.stream(files)
+                .map(file -> uploadFile(new UploadVo(file), finalModule, time))
+                .collect(Collectors.toList());
     }
 
     /**
      * 新建文件
      */
     @SneakyThrows
-    public static Tuple2<String, String> newFile(UploadModuleModel moduleEnum, LocalDateTime time, String fileSubFolder, String fileName) {
+    public static Tuple2<String, String> newFile(UploadModuleModel moduleEnum, LocalDateTime time, String fileSubFolder, String fileName, boolean forceToEmpty) {
         String relativePath = moduleEnum.getPath(time, fileSubFolder) + File.separator + fileName;
         String fullPath = commonProperties.getFiles().getSavePath() + relativePath;
-        new File(fullPath).createNewFile();
+        File file = new File(fullPath);
+        file.createNewFile();
+        if (forceToEmpty) {
+            FileUtil.writeUtf8String("", file);
+        }
         return new Tuple2<>((File.separator + "file" + relativePath).replaceAll("\\\\", File.separator), fullPath);
     }
 
@@ -100,7 +107,8 @@ public class FileUploadUtils {
         }
         //相对路径 (包括文件名,相对于根路径)
 //        String relativePath = moduleEnum.getPath(time) + File.separator + IdUtil.simpleUUID() + "_" + oriName;
-        String relativePath = moduleEnum.getPath(time, null) + File.separator + IdUtil.simpleUUID() + uploadFile.getSuffix();
+        String relativePath = moduleEnum.getPath(time,
+                null) + File.separator + IdUtil.simpleUUID() + uploadFile.getSuffix();
         File saveFile = new File(commonProperties.getFiles().getSavePath() + relativePath);
         boolean dirOK = true;
         if (!saveFile.getParentFile().exists()) {
@@ -130,7 +138,8 @@ public class FileUploadUtils {
     }
 
     public static String calSign(String fileName) {
-        String data = commonProperties.getFiles().getUploadPassword() + fileName + +System.currentTimeMillis() / 1000 / 1000;
+        String data = commonProperties.getFiles()
+                .getUploadPassword() + fileName + +System.currentTimeMillis() / 1000 / 1000;
         log.info("上传文件|签名前数据:" + data);
         return SecureUtil.md5(data);
     }
