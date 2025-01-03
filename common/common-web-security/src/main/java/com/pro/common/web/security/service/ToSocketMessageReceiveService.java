590778f87784a7817b44b4942d19b902a25d31dd
qaz4042 <404286846@qq.com>
2025-01-03 11:30:42 +0800

    feat: 第一版完成

--- common/common-web-security/src/main/java/com/pro/common/web/security/service/ToSocketMessageReceiveService.java
+++ common/common-web-security/src/main/java/com/pro/common/web/security/service/ToSocketMessageReceiveService.java
@@ -5,12 +5,14 @@ import com.pro.common.modules.api.dependencies.message.ToSocket;
 import com.pro.common.web.security.websocket.SocketSender;
 import com.pro.framework.message.IApplicationMessageReceiveService;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@Slf4j
 public class ToSocketMessageReceiveService implements IApplicationMessageReceiveService<ToSocket> {
     @Autowired
     private SocketSender socketSender;
@@ -19,6 +21,10 @@ public class ToSocketMessageReceiveService implements IApplicationMessageReceive
 
     @Override
     public void doReceive(ToSocket object, Message message) {
+        String line = object.getData();
+        if (line.contains("Building snowball-admin 2.0.0")) {
+            log.warn("===line {}", line);
+        }
         socketSender.send(object);
     }
 }
