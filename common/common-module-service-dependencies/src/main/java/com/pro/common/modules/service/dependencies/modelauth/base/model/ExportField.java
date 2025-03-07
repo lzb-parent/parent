fa8ed70915b02ad2f45d6ed61befe76403469c2b
Administrator <404286846@qq.com>
2025-03-07 16:47:33 +0800

    feat: 更新文档框架,修复一些 CommonData Bug

---
+++ common/common-module-service-dependencies/src/main/java/com/pro/common/modules/service/dependencies/modelauth/base/model/ExportField.java
@@ -0,0 +1,9 @@
+package com.pro.common.modules.service.dependencies.modelauth.base.model;
+
+import lombok.Data;
+
+@Data
+public class ExportField {
+    private String fieldName;
+    private String label;
+}
